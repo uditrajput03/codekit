5b4be12e18ae4327e4570f6d7710129de4040644
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-05-24 18:49:46 +0530

    Add static to ServiceCard

--- frontend/src/components/ServiceCard.jsx
+++ frontend/src/components/ServiceCard.jsx
@@ -1,29 +1,53 @@
 import { useNavigate } from "react-router-dom";
 
-const ServiceCard = ({ title = "Custom", description = "Get customized solutions", specs = [
-  "Frontend: React, TailwindCSS, Vite"
-  , "Backend - JS, TS"
-  , "Fullstack: FE + BE"
-  , "NextJS: Fullstack"
-  , "Website Maintainance"
-  , "Features Implementaion"] }) => {
+const ServiceCard = () => {
   const navigate = useNavigate()
   return (<>
     <div className='relative overflow-hidden max-w-xs rounded-xl border border-gray-800 p-[1px] backdrop-blur-3xl mb-20'>
       <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
       <div className='backdrop-blur-3xl'>
         <div className="flex p-2 flex-col justify-center items-center bg-black text-white text-center gap-1 border rounded-xl">
-          <h1 className="m-3 text-2xl font-semibold">{title}</h1>
-          <p className="font-light text-white sm:text-lg">{description}</p>
+          <h1 className="m-3 text-2xl font-semibold">Custom</h1>
+          <p className="font-light text-white sm:text-lg">Get customized solutions</p>
           <div className=" flex flex-col self-start gap-2">
-            {specs.map((e , index) => {
-              return <div key={index}>
-                <li className="flex items-center space-x-3">
-                  <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
-                  <span>{e}</span>
-                </li>
-              </div>
-            })}
+
+            <div>
+              <li className="flex items-center space-x-3">
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <span>Frontend: React, TailwindCSS, Vite</span>
+              </li>
+            </div>
+            <div>
+              <li className="flex items-center space-x-3">
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <span>Backend - JS, TS</span>
+              </li>
+            </div>
+            <div>
+              <li className="flex items-center space-x-3">
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <span>Fullstack: FE + BE</span>
+              </li>
+            </div>
+            <div>
+              <li className="flex items-center space-x-3">
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <span>NextJS: Fullstack</span>
+              </li>
+            </div>
+            <div>
+              <li className="flex items-center space-x-3">
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <span>Website Maintainance</span>
+              </li>
+            </div>
+            <div>
+              <li className="flex items-center space-x-3">
+                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                <span>Features Implementaion</span>
+              </li>
+            </div>
+
           </div>
           <div className="m-5">
 
