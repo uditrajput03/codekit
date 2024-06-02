923be1bdb2c039bbeba7d36d7de0d708e882b6e0
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-06-02 15:54:59 +0530

    Fix #17 Faq for mobile view

--- frontend/src/components/Faq.jsx
+++ frontend/src/components/Faq.jsx
@@ -1,7 +1,7 @@
 export default function Faq() {
     return (<>
         <div
-            className="relative w-full  px-6 pt-10 pb-8 my-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-xl sm:rounded-lg sm:px-10">
+            className="relative w-11/12 sm:w-full  px-6 pt-10 pb-8 my-8 shadow-xl ring-1 ring-gray-900/5 mx-auto sm:max-w-xl sm:rounded-lg sm:px-10">
             <div className="mx-auto px-5">
                 <div className="flex flex-col items-center">
                     <h2 className="mt-5 text-center text-3xl font-bold tracking-tight md:text-5xl">FAQ</h2>
