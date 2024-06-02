8bfc164f2e78689bdca7b219b4298faeab6a866a
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-06-02 16:04:42 +0530

    Fix pricebar for mobile

--- frontend/src/components/PriceBar.jsx
+++ frontend/src/components/PriceBar.jsx
@@ -8,19 +8,19 @@ export default function PriceBar({ setCard , cardData }) {
                 <button onClick={() => {
                     setCard(cardData.frontend)
                     setActive("frontend")
-                }} className={`px-10 p-4 border-r border-gray-200 rounded-s-lg  ${active == "frontend" ? "bg-gray-100" : "hover:bg-gray-50"}`} aria-current="page">Frontend</button>
+                }} className={`px-10 p-4 border-b sm:border-b-0 sm:border-r border-gray-200 sm:rounded-s-lg  ${active == "frontend" ? "bg-gray-100" : "hover:bg-gray-50"}`} aria-current="page">Frontend</button>
                 <button onClick={() => {
                     setCard(cardData.backend)
                     setActive("backend")
-                }} className={`px-10 p-4  border-r border-gray-200  hover:text-gray-700  ${active == "backend" ? "bg-gray-100" : "hover:bg-gray-50"} `}>Backend</button>
+                }} className={`px-10 p-4  border-b sm:border-r sm:border-b-0 border-gray-200  hover:text-gray-700  ${active == "backend" ? "bg-gray-100" : "hover:bg-gray-50"} `}>Backend</button>
                 <button onClick={() => {
                     setCard(cardData.fullstack)
                     setActive("fullstack")
-                }} className={`px-10 p-4  border-r border-gray-200  hover:text-gray-700 ${active == "fullstack" ? "bg-gray-100" : "hover:bg-gray-50"}`}>FullStack</button>
+                }} className={`px-10 p-4  border-b sm:border-r sm:border-b-0 border-gray-200  hover:text-gray-700 ${active == "fullstack" ? "bg-gray-100" : "hover:bg-gray-50"}`}>FullStack</button>
                 <button onClick={() => {
                     setCard(cardData.nextjs)
                     setActive("nextjs")
-                }} className={`px-10 p-4  border-gray-200 rounded-e-lg hover:text-gray-700 ${active == "nextjs" ? "bg-gray-100" : "hover:bg-gray-50"}`}>NextJS</button>
+                }} className={`px-10 p-4  border-gray-200 sm:rounded-e-lg hover:text-gray-700 border-b sm:border-none ${active == "nextjs" ? "bg-gray-100" : "hover:bg-gray-50"}`}>NextJS</button>
             </div>
 
         </div>
