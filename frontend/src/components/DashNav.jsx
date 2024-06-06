46f2e4d7ae7a31ed70056b141e996cbec948cf1f
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-06-06 18:10:23 +0530

    fixed DashNav in profile page

--- frontend/src/components/DashNav.jsx
+++ frontend/src/components/DashNav.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function DashNav({active , setActive}) {
     return (<>
-        <div className="flex justify-center sm:justify-normal sm:flex-col gap-2">
+        <div className="flex justify-center sm:justify-normal sm:flex-col sm:gap-2">
             <button onClick={() => {
                 // setCard(cardData.fullstack)
                 setActive("profile")
@@ -10,7 +10,7 @@ export default function DashNav({active , setActive}) {
             <button onClick={() => {
                 // setCard(cardData.fullstack)
                 setActive("orders")
-            }} className={`px-10 p-4  border-r border-gray-200  hover:text-gray-700  ${active == "orders" ? "bg-gray-100" : "hover:bg-gray-50"}`}>Orders</button>
+            }} className={`px-10 p-4  sm:border-r border-gray-200  hover:text-gray-700  ${active == "orders" ? "bg-gray-100" : "hover:bg-gray-50"}`}>Orders</button>
         </div>
     </>)
 }
