00f05812c35fc65d6683342ed28e3c6d951542a6
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-06-20 17:36:30 +0530

    CI/CD test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -170,7 +170,7 @@ app.post('newsletter', async (c) => {
       }
     })
     return c.json({
-      status: "success test 2"
+      status: "success test 3"
     })
   } catch (error: any) {
     if (error.code = "P2002") {
